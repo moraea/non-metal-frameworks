89257c848f60f3be9c8ce981256f4b771348ecde
Will <57969415+ASentientBot@users.noreply.github.com>
2022-04-03 19:35:16 -0400

    update Appearance.m to write to our own domain

--- SkyLight/Appearance.m
+++ SkyLight/Appearance.m
@@ -1,20 +1,26 @@
 // called in Appearance.prefPane, saves/loads the setting from defaults
-// Hedge's code will run under WindowServer and read it!
+// Hedge's code will run under Dock and read it!
 
 // TODO: put that in
 
-#define SWITCH_DEFAULTS @"moraea.appearance"
+#define SWITCH_DOMAIN @"moraea"
+#define SWITCH_KEY @"switches"
+
+NSUserDefaults* moraeaDefaults()
+{
+	return [NSUserDefaults.alloc initWithSuiteName:SWITCH_DOMAIN].autorelease;
+}
 
 void SLSSetAppearanceThemeSwitchesAutomatically(BOOL edi)
 {
 	trace(@"SLSSetAppearanceThemeSwitchesAutomatically %d",edi);
 	
-	[NSUserDefaults.standardUserDefaults setBool:edi forKey:SWITCH_DEFAULTS];
+	[moraeaDefaults() setBool:edi forKey:SWITCH_KEY];
 }
 
 BOOL SLSGetAppearanceThemeSwitchesAutomatically()
 {
-	BOOL result=[NSUserDefaults.standardUserDefaults boolForKey:SWITCH_DEFAULTS];
+	BOOL result=[moraeaDefaults() boolForKey:SWITCH_KEY];
 	
 	trace(@"SLSGetAppearanceThemeSwitchesAutomatically %d",result);
 	
