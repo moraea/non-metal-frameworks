d34403defdb1a9446c45403a5afd29c0250c3798
ASentientHedgehog <72932987+moosethegoose2213@users.noreply.github.com>
2022-04-03 20:59:09 -0500

    Light/Dark Mode Auto-Switching Reimplementation

--- SkyLight/Appearance.m
+++ SkyLight/Appearance.m
@@ -18,6 +18,7 @@ void SLSSetAppearanceThemeSwitchesAutomatically(BOOL edi)
 	[moraeaDefaults() setBool:edi forKey:SWITCH_KEY];
 }
 
+
 BOOL SLSGetAppearanceThemeSwitchesAutomatically()
 {
 	BOOL result=[moraeaDefaults() boolForKey:SWITCH_KEY];
@@ -25,4 +26,37 @@ BOOL SLSGetAppearanceThemeSwitchesAutomatically()
 	trace(@"SLSGetAppearanceThemeSwitchesAutomatically %d",result);
 	
 	return result;
+}
+
+
+void appearanceSetup()
+{
+	if([NSProcessInfo.processInfo.arguments[0] isEqualToString:@"/System/Library/CoreServices/Dock.app/Contents/MacOS/Dock"])
+	{
+		NSDate* fire=[NSDate dateWithTimeIntervalSinceNow:5]; // time until timer start
+		NSTimeInterval repeat=10; // checking interval
+		NSTimer* timer=[NSTimer.alloc initWithFireDate:fire interval:repeat repeats:true block:^(NSTimer* timer)
+		{
+			//define SLSSetAppearanceThemeLegacy as boolean, get value of SLSGetAppearanceThemeSwitchesAutomatically
+			void SLSSetAppearanceThemeLegacy(BOOL);
+			BOOL SLSGetAppearanceThemeSwitchesAutomatically();
+		
+			if(!SLSGetAppearanceThemeSwitchesAutomatically()){
+				return;
+			}
+			//get date formatted
+			NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+			    [dateFormatter setDateFormat:@"HH.mm"];
+			    NSString *strCurrentTime = [dateFormatter stringFromDate:[NSDate date]];
+
+			//if its past 6pm, dark mode - otherwise light
+			if ([strCurrentTime floatValue] >= 18.00 || [strCurrentTime floatValue]  <= 6.00){
+				//SLSSetAppearanceThemeLegacy(YES);
+				SLSSetAppearanceThemeLegacy(YES);
+		    } else{
+				SLSSetAppearanceThemeLegacy(NO);
+		    }
+		}];
+		[NSRunLoop.currentRunLoop addTimer:timer forMode:NSRunLoopCommonModes];
+	}
 }
