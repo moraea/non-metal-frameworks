07b60ae1a073010e9ca4f00d23b437c3bf86bad3
educovas <44532297+educovas@users.noreply.github.com>
2022-04-10 22:38:09 -0300

    Re-add notifications and increase timer interval time

--- SkyLight/Appearance.m
+++ SkyLight/Appearance.m
@@ -15,6 +15,7 @@ void SLSSetAppearanceThemeSwitchesAutomatically(BOOL edi)
 	trace(@"SLSSetAppearanceThemeSwitchesAutomatically %d",edi);
 	
 	[moraeaDefaults() setBool:edi forKey:SWITCH_KEY];
+	[NSDistributedNotificationCenter.defaultCenter postNotificationName:@"moraea.appearance" object:nil userInfo:@{@"switches":[NSNumber numberWithInt:edi]}];
 }
 
 // called below, reads default
@@ -34,26 +35,34 @@ void appearanceSetup()
 {
 	if([process isEqualToString:@"/System/Library/CoreServices/Dock.app/Contents/MacOS/Dock"])
 	{
-		NSDate* fire=[NSDate dateWithTimeIntervalSinceNow:5]; // time until timer start
-		NSTimeInterval repeat=10; // checking interval
-		NSTimer* timer=[NSTimer.alloc initWithFireDate:fire interval:repeat repeats:true block:^(NSTimer* timer)
-		{
-			if(!SLSGetAppearanceThemeSwitchesAutomatically()){
-				return;
-			}
-			//get date formatted
-			NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-			    [dateFormatter setDateFormat:@"HH.mm"];
-			    NSString *strCurrentTime = [dateFormatter stringFromDate:[NSDate date]];
+	[NSDistributedNotificationCenter.defaultCenter addObserverForName:@"moraea.appearance" object:nil queue:nil usingBlock:^(NSNotification* note)
+	{			
+		NSLog(@"moraea.appearance notif:\n %@", note);
+		if (((NSNumber*)note.userInfo[@"switches"]).boolValue == 1){
 
-			//if its past 6pm, dark mode - otherwise light
-			if ([strCurrentTime floatValue] >= 18.00 || [strCurrentTime floatValue]  <= 6.00){
-				//SLSSetAppearanceThemeLegacy(YES);
-				SLSSetAppearanceThemeLegacy(YES);
-		    } else{
-				SLSSetAppearanceThemeLegacy(NO);
-		    }
+			NSDate* fire=[NSDate dateWithTimeIntervalSinceNow:0.1]; // time until timer start
+			NSTimeInterval repeat=60; // checking interval
+			NSTimer* timer=[NSTimer.alloc initWithFireDate:fire interval:repeat repeats:true block:^(NSTimer* timer)
+				{
+					if(!SLSGetAppearanceThemeSwitchesAutomatically()){
+						return;
+					}
+					//get date formatted
+					NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+					    [dateFormatter setDateFormat:@"HH.mm"];
+					    NSString *strCurrentTime = [dateFormatter stringFromDate:[NSDate date]];
+
+					//if its past 6pm, dark mode - otherwise light
+					if ([strCurrentTime floatValue] >= 18.00 || [strCurrentTime floatValue]  <= 6.00){
+						//SLSSetAppearanceThemeLegacy(YES);
+						SLSSetAppearanceThemeLegacy(YES);
+		  	 	 } else{
+						SLSSetAppearanceThemeLegacy(NO);
+			 	   }
+				}];
+
+				[NSRunLoop.currentRunLoop addTimer:timer forMode:NSRunLoopCommonModes];
+			}
 		}];
-		[NSRunLoop.currentRunLoop addTimer:timer forMode:NSRunLoopCommonModes];
 	}
 }
