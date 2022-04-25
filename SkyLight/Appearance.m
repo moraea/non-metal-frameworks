071f53108fc844bcfa4655d7e6b88c4836e6550c
educovas <44532297+educovas@users.noreply.github.com>
2022-04-24 21:46:21 -0300

    Looks terrible but works

--- SkyLight/Appearance.m
+++ SkyLight/Appearance.m
@@ -9,18 +9,49 @@ NSUserDefaults* moraeaDefaults()
 }
 
 //the actual auto appearance "logic"
-setAppearance(){
+int setAppearance(){
 	//format time
 	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
 	[dateFormatter setDateFormat:@"HH.mm"];
 	NSString *strCurrentTime = [dateFormatter stringFromDate:[NSDate date]];	
 	
 	if ([strCurrentTime floatValue] >= 18.00 || [strCurrentTime floatValue]  <= 6.00){
+		if([[NSUserDefaults.standardUserDefaults stringForKey:@"AppleInterfaceStyle"] isEqualToString:@"Dark"])
+		{
+		}
+		else{
+		Class Transition=NSClassFromString(@"NSGlobalPreferenceTransition");
+		dispatch_async(dispatch_get_main_queue(),^()
+	{
+		id idOfTransition=[Transition transition];
+		trace(@"%@",idOfTransition);
+		[idOfTransition postChangeNotification:0 completionHandler:^(){}];
 		SLSSetAppearanceThemeLegacy(YES);
+		[idOfTransition waitForTransitionWithCompletionHandler:^()
+		{
+			return;
+		}];
+	});
+	}
  	} else{
+		if([[NSUserDefaults.standardUserDefaults stringForKey:@"AppleInterfaceStyle"] isEqualToString:@"Dark"])
+		{
+		Class Transition=NSClassFromString(@"NSGlobalPreferenceTransition");
+		dispatch_async(dispatch_get_main_queue(),^()
+	{
+		id idOfTransition=[Transition transition];
+		trace(@"%@",idOfTransition);
+		[idOfTransition postChangeNotification:0 completionHandler:^(){}];
 		SLSSetAppearanceThemeLegacy(NO);
- 	}
+		[idOfTransition waitForTransitionWithCompletionHandler:^()
+		{
+			return;
+		}];
+	});
+	}
+ 	}	
 }
+
 
 // called in Appearance.prefPane, writes default and immediately sets
 void SLSSetAppearanceThemeSwitchesAutomatically(BOOL edi)
@@ -48,9 +79,9 @@ BOOL SLSGetAppearanceThemeSwitchesAutomatically()
 // ASentientHedgehog's reimplementation of switching
 void appearanceSetup()
 {
-	if([process isEqualToString:@"/System/Library/CoreServices/Dock.app/Contents/MacOS/Dock"])
+	if([process isEqualToString:@"/System/Library/CoreServices/ControlCenter.app/Contents/MacOS/ControlCenter"])
 	{
-		NSDate* fire=[NSDate dateWithTimeIntervalSinceNow:1]; // time until timer start
+		NSDate* fire=[NSDate dateWithTimeIntervalSinceNow:0.1]; // time until timer start
 		NSTimeInterval repeat=60; // checking interval
 		NSTimer* timer=[NSTimer.alloc initWithFireDate:fire interval:repeat repeats:true block:^(NSTimer* timer)
 		{
