2fd8a59418f53f48521c531b35735f24c5b9df4b
educovas <44532297+educovas@users.noreply.github.com>
2022-04-26 21:46:55 +0200

    Update Appearance.m

--- SkyLight/Appearance.m
+++ SkyLight/Appearance.m
@@ -2,73 +2,71 @@
 
 #define SWITCH_DOMAIN @"moraea"
 #define SWITCH_KEY @"switches"
+BOOL SLSGetAppearanceThemeLegacy();
 
 NSUserDefaults* moraeaDefaults()
 {
 	return [NSUserDefaults.alloc initWithSuiteName:SWITCH_DOMAIN].autorelease;
 }
 
-//the actual auto appearance "logic"
-int setAppearance(){
-	//format time
+void setAppearance()
+{
+	Class Transition=NSClassFromString(@"NSGlobalPreferenceTransition");
+	dispatch_async(dispatch_get_main_queue(),^()
+	{
+		if(!SLSGetAppearanceThemeLegacy())
+		{
+			SLSSetAppearanceThemeLegacy(YES);
+		}
+		else
+		{
+			SLSSetAppearanceThemeLegacy(NO);
+		}
+		id idOfTransition=[Transition transition];
+		[idOfTransition postChangeNotification:0 completionHandler:^(){}];
+		[idOfTransition waitForTransitionWithCompletionHandler:^()
+		{
+			return;
+		}];
+	});
+}
+
+void checkMode(){
+
 	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
 	[dateFormatter setDateFormat:@"HH.mm"];
 	NSString *strCurrentTime = [dateFormatter stringFromDate:[NSDate date]];	
 	
-	if ([strCurrentTime floatValue] >= 18.00 || [strCurrentTime floatValue]  <= 6.00){
-		if([[NSUserDefaults.standardUserDefaults stringForKey:@"AppleInterfaceStyle"] isEqualToString:@"Dark"])
-		{
+	if([strCurrentTime floatValue] >= 18.00 || [strCurrentTime floatValue]  <= 6.00){
+		if(!SLSGetAppearanceThemeLegacy()){
+			setAppearance();
 		}
-		else{
-		Class Transition=NSClassFromString(@"NSGlobalPreferenceTransition");
-		dispatch_async(dispatch_get_main_queue(),^()
-	{
-		id idOfTransition=[Transition transition];
-		trace(@"%@",idOfTransition);
-		[idOfTransition postChangeNotification:0 completionHandler:^(){}];
-		SLSSetAppearanceThemeLegacy(YES);
-		[idOfTransition waitForTransitionWithCompletionHandler:^()
-		{
-			return;
-		}];
-	});
-	}
- 	} else{
-		if([[NSUserDefaults.standardUserDefaults stringForKey:@"AppleInterfaceStyle"] isEqualToString:@"Dark"])
-		{
-		Class Transition=NSClassFromString(@"NSGlobalPreferenceTransition");
-		dispatch_async(dispatch_get_main_queue(),^()
-	{
-		id idOfTransition=[Transition transition];
-		trace(@"%@",idOfTransition);
-		[idOfTransition postChangeNotification:0 completionHandler:^(){}];
-		SLSSetAppearanceThemeLegacy(NO);
-		[idOfTransition waitForTransitionWithCompletionHandler:^()
-		{
-			return;
-		}];
-	});
-	}
- 	}	
+ 	} 
+	else{
+		if(SLSGetAppearanceThemeLegacy()){
+			setAppearance();
+		}
+ 	}
+	[dateFormatter release];
 }
 
-
-// called in Appearance.prefPane, writes default and immediately sets
+// Writes default and immediately sets
 void SLSSetAppearanceThemeSwitchesAutomatically(BOOL edi)
-{
-	trace(@"SLSSetAppearanceThemeSwitchesAutomatically %d",edi);
-	
+{	
 	[moraeaDefaults() setBool:edi forKey:SWITCH_KEY];
 	
 	//return if auto-appearance isn't enabled
-	if (edi == 0){
+	if (edi == 0)
+	{
 		return;
-	} else{
-		setAppearance();
+	} 
+	else
+	{
+		checkMode();
 	}
 }
 
-// called below, reads default
+// Reads default
 BOOL SLSGetAppearanceThemeSwitchesAutomatically()
 {
 	BOOL result=[moraeaDefaults() boolForKey:SWITCH_KEY];
@@ -76,20 +74,22 @@ BOOL SLSGetAppearanceThemeSwitchesAutomatically()
 	return result;
 }
 
-// ASentientHedgehog's reimplementation of switching
+// ASentientHedgehog's timer
 void appearanceSetup()
 {
 	if([process isEqualToString:@"/System/Library/CoreServices/ControlCenter.app/Contents/MacOS/ControlCenter"])
 	{
 		NSDate* fire=[NSDate dateWithTimeIntervalSinceNow:0.1]; // time until timer start
-		NSTimeInterval repeat=60; // checking interval
+		NSTimeInterval repeat=30; // checking interval
 		NSTimer* timer=[NSTimer.alloc initWithFireDate:fire interval:repeat repeats:true block:^(NSTimer* timer)
 		{
 			if(!SLSGetAppearanceThemeSwitchesAutomatically())
 			{
 				return;
-			} else{
-				setAppearance();
+			}
+			else
+			{
+				checkMode();
 			}
 		}];
 		[NSRunLoop.currentRunLoop addTimer:timer forMode:NSRunLoopCommonModes];
