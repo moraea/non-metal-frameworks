13b6f8441c17c27c141c7c89d58e8d04bd0e0257
ASentientHedgehog <72932987+moosethegoose2213@users.noreply.github.com>
2022-05-25 16:47:57 -0500

    make look more goober

--- SkyLight/EnableTransparency.m
+++ SkyLight/EnableTransparency.m
@@ -1,10 +1,11 @@
 NSString *_AXInterfaceGetReduceTransparencyEnabled();
-int fake__AXInterfaceGetReducedTransparencyEnabled(){
-	
+
+int fake__AXInterfaceGetReducedTransparencyEnabled(){	
     if([NSUserDefaults.standardUserDefaults boolForKey:@"Moraea_EnableTransparency"]){
         return 0;
     } else {
         return _AXInterfaceGetReduceTransparencyEnabled();
     }
 }
+
 DYLD_INTERPOSE(fake__AXInterfaceGetReducedTransparencyEnabled,_AXInterfaceGetReduceTransparencyEnabled)
