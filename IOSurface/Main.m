d03b8e236cec16db747ab115516b598d1d7d672c
Amy <asentientbot@gmail.com>
2023-03-11 14:08:14 -0500

    update readme

--- IOSurface/Main.m
+++ IOSurface/Main.m
@@ -3,6 +3,11 @@
 
 #ifdef CAT
 
+// TODO: downgrading IOSurface to 10.14.6 (TS2 patchset) fixes the underlying issue for me
+// but Edu can't replicate this on his identical Zoe...
+// confirmed with IOConnectCallMethod that i get 8192 after downgrading, he still gets INT_MAX
+// need to reverse IOSurface.kext, but it's quite opaque...
+
 size_t IOSurfaceGetPropertyMaximu$(CFStringRef);
 
 size_t IOSurfaceGetPropertyMaximum(CFStringRef property)
@@ -10,8 +15,8 @@ size_t IOSurfaceGetPropertyMaximum(CFStringRef property)
 	size_t real=IOSurfaceGetPropertyMaximu$(property);
 	if(real==INT_MAX)
 	{
-		// TODO: actually fetch this somehow?
-		// returned by working IOSurface on Zoe
+		// TODO: actually fetch the value somehow?
+		// returned by working IOSurface on Zoe, but could vary on different GPUs
 		
 		return 0x2000;
 	}
