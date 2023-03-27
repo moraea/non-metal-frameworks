5087eab340d51db61fe5aaea057f01ed7b502275
Amy <asentientbot@gmail.com>
2023-03-27 13:34:26 -0400

    add TODOs

--- SkyLight/MenuBar2.m
+++ SkyLight/MenuBar2.m
@@ -1,5 +1,7 @@
-// testing some new menu bar hacks (proper selections, auto text color)
-// TODO: currently very pre-alpha quality!
+// new menu bar hacks (proper selections, auto text color)
+// TODO: test thoroughly, then enable by default, then replace MenuBar entirely
+// TODO: correct bar background on fullscreen windows
+// TODO: tweak color threshold to exactly match Metal
 
 #define MENUBAR_DARK_FORMAT @"Amy.MenuBar2.DarkText.%d"
 #define MENUBAR_DARK_NOTE @"Amy.MenuBar2.DarkTextChanged"
