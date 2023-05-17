e45f1d83e05a7898a8d41b0d28fe0aefc4a4c716
Amy <57969415+ASentientBot@users.noreply.github.com>
2023-05-17 11:50:19 -0400

    add SL*Preflight*Access stubs (#30)

---
+++ SkyLight/Preflight.m
@@ -0,0 +1,51 @@
+// old SkyLight doesn't need these checks, so just tell apps to go ahead
+
+BOOL SLPreflightListenEventAccess()
+{
+	return true;
+}
+BOOL SLPreflightPostEventAccess()
+{
+	return true;
+}
+BOOL SLPreflightScreenCaptureAccess()
+{
+	return true;
+}
+BOOL SLRequestListenEventAccess()
+{
+	return true;
+}
+BOOL SLRequestPostEventAccess()
+{
+	return true;
+}
+BOOL SLRequestScreenCaptureAccess()
+{
+	return true;
+}
+
+BOOL SLSPreflightListenEventAccess()
+{
+	return true;
+}
+BOOL SLSPreflightPostEventAccess()
+{
+	return true;
+}
+BOOL SLSPreflightScreenCaptureAccess()
+{
+	return true;
+}
+BOOL SLSRequestListenEventAccess()
+{
+	return true;
+}
+BOOL SLSRequestPostEventAccess()
+{
+	return true;
+}
+BOOL SLSRequestScreenCaptureAccess()
+{
+	return true;
+}
