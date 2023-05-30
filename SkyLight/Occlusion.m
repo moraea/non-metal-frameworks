3dd65001327999edf9f9228d7885bf5622091c95
Amy <asentientbot@gmail.com>
2023-05-29 21:01:57 -0400

    retry Occlusion.m swizzles on AppKit load
    work around SL initializer running before AppKit framework loads

--- SkyLight/Occlusion.m
+++ SkyLight/Occlusion.m
@@ -67,9 +67,22 @@ void fake_viewDidMoveToWindow(NSObject* rdi_self,SEL rsi_sel)
 
 void occlusionSetup()
 {
-	swizzleImp(@"NSWindow",@"_setWindowNumber:",true,(IMP)fake__setWindowNumber,(IMP*)&real__setWindowNumber);
+	BOOL retry=!swizzleImp(@"NSWindow",@"_setWindowNumber:",true,(IMP)fake__setWindowNumber,(IMP*)&real__setWindowNumber);
 	
-	swizzleImp(@"NSOcclusionDetectionView",@"validateNoOcclusionSinceToken:",true,(IMP)fake_validateNoOcclusionSinceToken,NULL);
-	swizzleImp(@"NSOcclusionDetectionView",@"isOccluded",true,(IMP)fake_isOccluded,NULL);
-	swizzleImp(@"NSOcclusionDetectionView",@"viewDidMoveToWindow",true,(IMP)fake_viewDidMoveToWindow,(IMP*)&real_viewDidMoveToWindow);
+	if(!retry)
+	{
+		swizzleImp(@"NSOcclusionDetectionView",@"validateNoOcclusionSinceToken:",true,(IMP)fake_validateNoOcclusionSinceToken,NULL);
+		swizzleImp(@"NSOcclusionDetectionView",@"isOccluded",true,(IMP)fake_isOccluded,NULL);
+		swizzleImp(@"NSOcclusionDetectionView",@"viewDidMoveToWindow",true,(IMP)fake_viewDidMoveToWindow,(IMP*)&real_viewDidMoveToWindow);
+	}
+	
+	if(retry)
+	{
+		[NSNotificationCenter.defaultCenter addObserverForName:@"NSApplicationWillFinishLaunchingNotification" object:nil queue:nil usingBlock:^(NSNotification* note)
+		{
+			// trace(@"retrying occlusion swizzles");
+			
+			occlusionSetup();
+		}];
+	}
 }
