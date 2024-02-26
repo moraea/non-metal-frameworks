262b0451bc63d51d1d3c45337c26ac573a4a0598
Amy <asentientbot@gmail.com>
2024-02-25 23:06:26 -0500

    edu pid hack
    bastardized stolen version of Edu's fix for this dp3 issue because he didn't push it to github himself yet for some reason
    
    Co-Authored-By: Eduardo Covas <44532297+educovas@users.noreply.github.com>

--- SkyLight/BServices.m
+++ SkyLight/BServices.m
@@ -5,10 +5,26 @@ id fake_invalidate()
 	return nil;
 }
 
+
+id (*real_information)(id,SEL,int);
+
+id fake_information(id rdi_self,SEL rsi_sel,int edx_pid)
+{
+	if(edx_pid==0)
+	{
+		// bruh
+		
+		edx_pid=getpid();
+	}
+	
+	return real_information(rdi_self,rsi_sel,edx_pid);
+}
+
 void bservicesSetup()
 {
 	if([process isEqualToString:@"/System/Library/CoreServices/loginwindow.app/Contents/MacOS/loginwindow"])
 	{
 		swizzleImp(@"NSMachPort",@"invalidate",true,(IMP)fake_invalidate,NULL);
+		swizzleImp(@"PKManager",@"informationForPlugInWithPid:",true,fake_information,&real_information);
 	}
 }
